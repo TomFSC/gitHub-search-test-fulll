da8d6cf285da769f27d79c7e16762e05e750005f
Thomas FAISCA <faisca.thomas@gmail.com>
2023-07-27 22:05:15 +0200

    🚧 Create GithubUsers.tsx

--- src/components/pages/githubSearch/GitHubSearchPage.tsx
+++ src/components/pages/githubSearch/GitHubSearchPage.tsx
@@ -1,3 +1,4 @@
+import GithubUsers from './GithubUsers/GithubUsers'
 import SearchSection from './SearchSection/SearchSection'
 import TopBar from './TopBar/TopBar'
 import './githubSearchPage.css'
@@ -7,19 +8,7 @@ function GitHubSearchPage() {
     <div className="container">
         <TopBar/>
         <SearchSection/>
-        <div className="github-profile-container">
-            <div className="card">
-                <input type="checkbox" />
-                <div className="avatar">
-                    <img src="" alt="" />
-                </div>
-                <span>ID</span>
-                <span>Login</span>
-                <button>View profile</button>
-
-            </div>
-            CARDS
-        </div>
+        <GithubUsers/>
     </div>
   )
 }
---
+++ src/components/pages/githubSearch/GithubUsers/GithubUsers.tsx
@@ -0,0 +1,21 @@
+import "./githubUsers.css"
+
+function GithubUsers() {
+  return (
+    <div className="github-profile-container">
+            <div className="card">
+                <input type="checkbox" />
+                <div className="avatar">
+                    <img src="" alt="" />
+                </div>
+                <span>ID</span>
+                <span>Login</span>
+                <button>View profile</button>
+
+            </div>
+            CARDS
+        </div>
+  )
+}
+
+export default GithubUsers
