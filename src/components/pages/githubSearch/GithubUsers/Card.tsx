8dddef5661562b5b9e5730f13444b672779dcbc0
Thomas FAISCA <faisca.thomas@gmail.com>
2023-07-29 12:07:52 +0200

    ♻️ Create Card

---
+++ src/components/pages/githubSearch/GithubUsers/Card.tsx
@@ -0,0 +1,22 @@
+function Card({ user, onChange, isChecked }: any) {
+  const { login, avatar_url, id } = user;
+
+  return (
+    <div className="card">
+      <input
+        type="checkbox"
+        onChange={() => onChange(user)}
+        checked={isChecked}
+      />
+      <div className="avatar">
+        <img src={avatar_url} alt="profile-avatar" />
+      </div>
+      <span>{id}</span>
+      <br />
+      <span>{login}</span>
+      <button>View profile</button>
+    </div>
+  );
+}
+
+export default Card;
