6c8a64ef730aa73c49abf0477f660d55959b0d19
Thomas FAISCA <faisca.thomas@gmail.com>
2023-08-11 15:14:41 +0200

    ✅ Update useDebounce tests

--- src/hooks/__test__/useDebounce.test.ts
+++
@@ -1,8 +0,0 @@
-import { renderHook } from "@testing-library/react";
-import { useDebounce } from "../useDebounce";
-
-test("useDebounce with value", async () => {
-  const value = "TomFSC";
-  const { result } = renderHook(() => useDebounce(value));
-  expect(result.current).toEqual("TomFSC");
-});
---
+++ src/hooks/__test__/useDebounce.test.tsx
@@ -0,0 +1,52 @@
+import { act, renderHook } from "@testing-library/react";
+import { useDebounce } from "../useDebounce";
+
+describe("useDebounce", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
+  test("should debounce the input value", () => {
+    const { result, rerender } = renderHook(
+      ({ value, delay }) => useDebounce(value, delay),
+      {
+        initialProps: { value: "initial", delay: 500 },
+      }
+    );
+
+    expect(result.current).toBe("initial");
+
+    rerender({ value: "updated", delay: 500 });
+
+    expect(result.current).toBe("initial");
+
+    act(() => {
+      jest.advanceTimersByTime(500);
+    });
+
+    expect(result.current).toBe("updated");
+  });
+
+  test("should clear the timeout on unmount", () => {
+    const { result, unmount } = renderHook(
+      ({ value, delay }) => useDebounce(value, delay),
+      {
+        initialProps: { value: "initial", delay: 500 },
+      }
+    );
+
+    expect(result.current).toBe("initial");
+
+    unmount();
+
+    act(() => {
+      jest.advanceTimersByTime(500);
+    });
+
+    expect(result.current).toBe("initial");
+  });
+});
