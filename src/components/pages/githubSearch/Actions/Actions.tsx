285ef08a4d82927fb0e8477f7f9d1aab0e98b432
Thomas FAISCA <faisca.thomas@gmail.com>
2023-08-13 06:35:08 +0200

    🎨  Clean Actions (remove tests)

--- src/components/pages/githubSearch/Actions/Actions.tsx
+++ src/components/pages/githubSearch/Actions/Actions.tsx
@@ -3,20 +3,21 @@ import { SearchContext } from "../../../../context/SearchContext";
 import Search from "../../../reusable-ui/Search";
 import EditPanel from "./EditPanel/EditPanel";
 
+import { USER_SEARCH_PLACEHOLDER } from "../../../../ts/constants";
 import "./actions.css";
 
 function Actions() {
   const { isEditMode, searchValue, handleSearch } = useContext(SearchContext);
 
   return (
-    <div data-testid="actions-section" className="actions-section">
+    <section>
       <Search
-        placeholder={"Search for user"}
+        placeholder={USER_SEARCH_PLACEHOLDER}
         onChange={handleSearch}
         value={searchValue}
       />
       {isEditMode && <EditPanel />}
-    </div>
+    </section>
   );
 }
 
