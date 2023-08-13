0226ece7c245408484f7beb51b50aeb8c7a64b26
Thomas FAISCA <faisca.thomas@gmail.com>
2023-08-13 11:09:00 +0200

    ♻️ use constants & change naming

--- src/hooks/__test__/useDebounce.test.tsx
+++ src/hooks/__test__/useDebounce.test.tsx
@@ -1,7 +1,15 @@
 import { act, renderHook } from "@testing-library/react";
 import { useDebounce } from "../useDebounce";
+import { DEBOUNCE_DELAY } from "../../ts/constants";
+
+const INITIAL_SEARCH_VALUE = "initial";
+const UPDATED_SEARCH_VALUE = "updated";
 
 describe("useDebounce", () => {
+  const initialPropsForUseDebounce = {
+    initialProps: { value: INITIAL_SEARCH_VALUE, delay: DEBOUNCE_DELAY },
+  };
+
   beforeEach(() => {
     jest.useFakeTimers();
   });
@@ -10,43 +18,51 @@ describe("useDebounce", () => {
     jest.clearAllTimers();
   });
 
-  test("should debounce the input value", () => {
+  test("Should return the initial value", () => {
+    const { result } = renderHook(
+      ({ value, delay }) => useDebounce(value, delay),
+      initialPropsForUseDebounce
+    );
+    const debouncedValue = result.current;
+
+    expect(debouncedValue).toBe(INITIAL_SEARCH_VALUE);
+  });
+
+  test("Should debounce the input value", () => {
     const { result, rerender } = renderHook(
       ({ value, delay }) => useDebounce(value, delay),
-      {
-        initialProps: { value: "initial", delay: 500 },
-      }
+      initialPropsForUseDebounce
     );
 
-    expect(result.current).toBe("initial");
+    const debouncedValueAfterRerender = result.current;
 
-    rerender({ value: "updated", delay: 500 });
+    rerender({ value: UPDATED_SEARCH_VALUE, delay: DEBOUNCE_DELAY });
 
-    expect(result.current).toBe("initial");
+    expect(debouncedValueAfterRerender).toBe(INITIAL_SEARCH_VALUE);
 
     act(() => {
-      jest.advanceTimersByTime(500);
+      jest.advanceTimersByTime(DEBOUNCE_DELAY);
     });
 
-    expect(result.current).toBe("updated");
+    const debouncedValueAfterRerenderAndDebounce = result.current;
+
+    expect(debouncedValueAfterRerenderAndDebounce).toBe(UPDATED_SEARCH_VALUE);
   });
 
   test("should clear the timeout on unmount", () => {
     const { result, unmount } = renderHook(
       ({ value, delay }) => useDebounce(value, delay),
-      {
-        initialProps: { value: "initial", delay: 500 },
-      }
+      initialPropsForUseDebounce
     );
 
-    expect(result.current).toBe("initial");
+    expect(result.current).toBe(INITIAL_SEARCH_VALUE);
 
     unmount();
 
     act(() => {
-      jest.advanceTimersByTime(500);
+      jest.advanceTimersByTime(DEBOUNCE_DELAY);
     });
 
-    expect(result.current).toBe("initial");
+    expect(result.current).toBe(INITIAL_SEARCH_VALUE);
   });
 });
