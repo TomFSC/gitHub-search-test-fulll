2704731b9ca513b12abf0bf83ba798381dd06a23
Thomas FAISCA <faisca.thomas@gmail.com>
2023-08-15 10:02:57 +0200

    ✅ Remove App test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders search page correctly", () => {
-  render(<App />);
-  const container = screen.getByTestId("main-container");
-  expect(container).toBeInTheDocument();
-});
